02c696d2b8ece706ea50d15a2d4a5939a63811cd
LouisCoutel <louiscoutel75@gmail.com>
2026-04-20 20:33:18 +0200

    refac: formatting

--- src/app/requests.ts
+++ src/app/requests.ts
@@ -3,17 +3,26 @@ import { decode } from "@googlemaps/polyline-codec";
 import { HttpClient } from "@angular/common/http";
 import { Round, RoundSummary } from "./typing";
 
-const rootUrl = "https://exo-api.dev.unicofrance.com/"
+const rootUrl = "https://exo-api.dev.unicofrance.com/";
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class RequestHandler {
-  constructor(private http: HttpClient) { }
-  allRounds() { return this.http.get<RoundSummary[]>(`${rootUrl}/round/`, { responseType: 'json' }) }
-  oneRound(id: string) { return this.http.get<Round>(`${rootUrl}/round/${id}`, { responseType: 'json' }) }
-  roundTracking(id: string) {
-    return this.http.get(`${rootUrl}/round/${id}/track`, { responseType: 'text' }).subscribe((text) => { return decode(text) });
-  }
+	constructor(private http: HttpClient) {}
+	allRounds() {
+		return this.http.get<RoundSummary[]>(`${rootUrl}/round/`, {
+			responseType: "json",
+		});
+	}
+	oneRound(id: string) {
+		return this.http.get<Round>(`${rootUrl}/round/${id}`, {
+			responseType: "json",
+		});
+	}
+	roundTracking(id: string) {
+		return this.http
+			.get(`${rootUrl}/round/${id}/track`, { responseType: "text" })
+			.subscribe((text) => {
+				return decode(text);
+			});
+	}
 }
-
-
