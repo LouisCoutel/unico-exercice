cb254fc372c8297fe514c8c0366f6c946f83f33e
LouisCoutel <louiscoutel75@gmail.com>
2026-04-20 17:39:38 +0200

    refac: remove bad tests from app.spec

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,22 +14,4 @@ describe('App', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  it('should retrieve rounds', async () => {
-    const rounds = await allRounds()
-    expect(rounds.success).toBeTruthy();
-  });
-  it('should retrieve specific round', async () => {
-    const rounds = await allRounds()
-    if (rounds.success) {
-      const test_round = await oneRound(rounds.value[0].id)
-      expect(test_round.success).toBeTruthy
-    }
-  })
-  it('should retrieve specific round tracking geodata', async () => {
-    const rounds = await allRounds()
-    if (rounds.success) {
-      const test_round = await roundTracking(rounds.value[0].id)
-      expect(test_round.success).toBeTruthy
-    }
-  })
 });
