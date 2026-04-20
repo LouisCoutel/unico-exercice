e426d41048337e8b9b79d675e7b1beda4090ccc2
LouisCoutel <louiscoutel75@gmail.com>
2026-04-20 18:57:37 +0200

    feat, refac: use simple datasource instead of Observable to implement sorting more easily

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1,23 +1,27 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { MatTableModule } from "@angular/material/table";
-import { RequestHandler } from "../../requests";
-import { interval, Observable, startWith, switchMap } from "rxjs";
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from "@angular/core";
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 import { RoundSummary } from "../../typing";
+import { RequestHandler } from "../../requests";
 
 
 @Component({
   selector: 'round-table',
   templateUrl: './table.html',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatSortModule],
 })
-export class RoundTable implements OnInit {
-  rounds$!: Observable<RoundSummary[]>;
+export class RoundTable implements OnInit, AfterViewInit {
+  rounds = new MatTableDataSource<RoundSummary>();
+  dataSource = new MatTableDataSource<RoundSummary>();
   displayedColumns: string[] = ['id', 'name', 'vehicle_name', 'driver_name'];
   protected readonly requestHandler = inject(RequestHandler)
+
+  @ViewChild(MatSort) sort!: MatSort;
+
   ngOnInit(): void {
-    this.rounds$ = interval(60000).pipe(
-      startWith(0),
-      switchMap(() => this.requestHandler.allRounds())
-    );
+    this.requestHandler.allRounds().subscribe(data => this.rounds.data = data)
+  }
+  ngAfterViewInit() {
+    this.rounds.sort = this.sort;
   }
 }
