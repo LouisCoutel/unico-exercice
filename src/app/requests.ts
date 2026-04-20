522e0a5b963beee61a609f0970e5963792c7bbba
LouisCoutel <louiscoutel75@gmail.com>
2026-04-20 17:21:07 +0200

    refac, feat: create a service using angular HttpClient rather than custom fetch calls

--- src/app/requests.ts
+++ src/app/requests.ts
@@ -1,28 +1,19 @@
-import { Result } from "./errors"
-import type { components } from "./schema"
-
-type RoundSummary = components["schemas"]["RoundSummary"]
-type Round = components["schemas"]["Round"]
-
-import { decode, encode, LatLngTuple } from "@googlemaps/polyline-codec";
+import { Injectable } from "@angular/core";
+import { decode } from "@googlemaps/polyline-codec";
+import { HttpClient } from "@angular/common/http";
+import { Round, RoundSummary } from "./typing";
 
 const rootUrl = "https://exo-api.dev.unicofrance.com/"
 
-const fetchAny = async <T>(url: string, parser: (res: Response) => Promise<T>): Promise<Result<T, number>> => {
-  const res = await fetch(url)
-  if (res.ok) {
-    const value = await parser(res)
-    return { success: res.ok, value: value }
+
+@Injectable({ providedIn: 'root' })
+export class RequestHandler {
+  constructor(private http: HttpClient) { }
+  allRounds() { return this.http.get<RoundSummary[]>(`${rootUrl}/round/`, { responseType: 'json' }) }
+  oneRound(id: string) { return this.http.get<Round>(`${rootUrl}/round/${id}`, { responseType: 'json' }) }
+  roundTracking(id: string) {
+    return this.http.get(`${rootUrl}/round/${id}/track`, { responseType: 'text' }).subscribe((text) => { return decode(text) });
   }
-  return { success: res.ok, error: res.status }
 }
 
-const polylineParse = async (res: Response) => {
-  const text = await res.text()
-  return decode(text)
-
-}
-export const allRounds = () => fetchAny<RoundSummary[]>(`${rootUrl}/round/`, res => res.json())
-export const oneRound = (id: string) => fetchAny<Round>(`${rootUrl}/round/${id}`, res => res.json())
-export const roundTracking = (id: string) => fetchAny<LatLngTuple[]>(`${rootUrl}/round/${id}/track`, polylineParse)
 
