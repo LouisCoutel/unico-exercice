fe3c0252f7504cfb0733f932671d17182e189ed1
LouisCoutel <louiscoutel75@gmail.com>
2026-04-20 20:33:45 +0200

    feat: some styling and setting up router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { RoundTable } from './components/table/table';
-
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { RoundTable } from "./components/table/table";
 
 @Component({
-  selector: 'app-root',
-  imports: [RoundTable],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('unico');
+	protected readonly title = signal("unico");
 }
