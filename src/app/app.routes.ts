e56b92ba4fc156ce5e4989b9c14207e69d96cdda
LouisCoutel <louiscoutel75@gmail.com>
2026-04-21 19:31:49 +0200

    fix: wildcard at the end of routes list (last resort only)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RoundPage } from "./components/roundPage/roundPage";
 import { RoundTable } from "./components/table/table";
 
 export const routes: Routes = [
-  { path: "**", redirectTo: "rounds" },
   { path: "round/:id", component: RoundPage },
   { path: "rounds", component: RoundTable },
+  { path: "**", redirectTo: "rounds" },
 ];
