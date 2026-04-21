8d8722cfc9d814bbb695eea0e7945374090cc153
LouisCoutel <louiscoutel75@gmail.com>
2026-04-21 18:50:24 +0200

    feat: proper column names according to specs

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1,34 +1,39 @@
-import { AfterViewInit, Component, inject, OnInit, ViewChild } from "@angular/core";
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import {
+  type AfterViewInit,
+  Component,
+  inject,
+  type OnInit,
+  ViewChild,
+} from "@angular/core";
+import { MatSort, MatSortModule } from "@angular/material/sort";
 import { MatTableDataSource, MatTableModule } from "@angular/material/table";
-import { RoundSummary } from "../../typing";
 import { RequestHandler } from "../../requests";
-import { columnInfo } from "./typing";
-
+import type { RoundSummary } from "../../typing";
+import type { columnInfo } from "./typing";
 
 @Component({
-  selector: 'round-table',
-  templateUrl: './table.html',
+  selector: "round-table",
+  templateUrl: "./table.html",
   imports: [MatTableModule, MatSortModule],
 })
 export class RoundTable implements OnInit, AfterViewInit {
   rounds = new MatTableDataSource<RoundSummary>();
   columns: columnInfo[] = [
-    { id: "id", traduction: "Id" },
-    { id: "name", traduction: "Nom" },
-    { id: "driver_name", traduction: "Conducteur" },
-    { id: "vehicle_name", traduction: "Véhicule" },
-    { id: "duration_s", traduction: "Durée (s)" },
-    { id: "distance_m", traduction: "Distance (m)" }
-  ]
-  displayedColumns = this.columns.map(c => c.id);
-  protected readonly requestHandler = inject(RequestHandler)
-
+    { id: "name", traduction: "Nom de la tournée" },
+    { id: "driver_name", traduction: "Nom du conducteur" },
+    { id: "vehicle_name", traduction: "Nom du véhicule" },
+    { id: "duration_s", traduction: "Durée" },
+    { id: "distance_m", traduction: "Distance parcourue" },
+  ];
+  displayedColumns = this.columns.map((c) => c.id);
+  protected readonly requestHandler = inject(RequestHandler);
 
   @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
-    this.requestHandler.allRounds().subscribe(data => this.rounds.data = data)
+    this.requestHandler
+      .allRounds()
+      .subscribe((data) => (this.rounds.data = data));
   }
   ngAfterViewInit() {
     this.rounds.sort = this.sort;
